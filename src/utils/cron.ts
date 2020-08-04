afc5b80563a0831b9de385d3259f368f7031aa11
MrMarble <4268580+MrMarble@users.noreply.github.com>
2020-08-04 15:49:30 +0200

    huge bug

--- src/utils/cron.ts
+++ src/utils/cron.ts
@@ -20,7 +20,7 @@ async function task(): Promise<void> {
     getGame(game.slug, chnl);
     const newGame = await chnl.recv();
 
-    if (newGame.isCracked() || newGame.releaseDate.getTime() >= Date.now()) {
+    if (newGame.isCracked() || newGame.releaseDate.getTime() <= Date.now()) {
       logger.info(`cron: ${game.title} is ${newGame.isCracked() ? 'cracked' : 'released'}!`);
 
       const users = await UserModel.find({ subscriptions: newGame }).exec();
